f7e008782966e46a16c648d2c4b711d4042c4a36
MdNadiruzzamanNibras <nadiruzzamannibras@gmail.com>
2023-05-19 11:30:09 +0600

    vercel link add

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,63 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { useEffect, useState } from 'react';
 import Table from 'react-bootstrap/Table';
 
 function App() {
-
-
+const [datas, setData] =useState([])
+useEffect(()=>{
+  fetch('https://football-backend.vercel.app/getMatchStats')
+  .then(res=>res.json())
+  .then(d => setData(d))
+},[])
+console.log(datas);
   return (
     <div>
-      <Table striped bordered hover>
+  {
+    datas.map(data=><Table key={data.matchId} striped bordered hover>
     
-    <tbody>
-      <tr>
-        <td>1</td>
-        <td>Mark</td>
-        <td>Otto</td>
-        <td>@mdo</td>
-      </tr>
-      
-      
-    </tbody>
-  </Table>
+      <tbody>
+        <tr>
+          <td>Date</td>
+          <td colSpan={2} className='ps-2'>{data.date}</td>
+          
+        </tr>
+        <tr>
+          <td></td>
+          <td> Team 1</td>
+          <td> Team 2</td>
+        </tr>
+        <tr>
+          <td>1</td>
+          <td >Mark</td>
+          <td >Otto</td>
+        </tr>
+        <tr>
+          <td>1</td>
+          <td>Mark</td>
+          <td>Otto</td>
+        </tr>
+        <tr>
+          <td>1</td>
+          <td>Mark</td>
+          <td>Otto</td>
+        </tr>
+        <tr>
+          <td>1</td>
+          <td>Mark</td>
+          <td>Otto</td>
+        </tr>
+        <tr>
+          <td>1</td>
+          <td>Mark</td>
+          <td>Otto</td>
+        </tr>
+        
+        
+      </tbody>
+    </Table>)
+  }
+
+     
     </div>
   )
 }
